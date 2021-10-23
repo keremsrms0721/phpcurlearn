d2df5d00c62e4186ee3c4d0378d22821e08703ec
keremsrms0721 <withkerem.python@gmail.com>
2021-10-23 12:36:10 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Curl işlemleri için sistem</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/resume.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-block d-lg-none">Curl sistem</span>
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/res.png" alt="">
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+      	<li class="nav-item">
+          <a class="nav-link js-scroll-trigger" target="_blank" href="islemler.php?islem=normal">Normal Veri</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" target="_blank" href="islemler.php?islem=kayitlar">Db işlemleri</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" target="_blank" href="islemler.php?islem=oturum">Oturum</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link js-scroll-trigger"  target="_blank" href="islemler.php?islem=dosyalar">Dosyalar</a>
+        </li>
+      
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container-fluid p-0 text-center">
+  
+ 
+<h1 class="mt-5 ">CURL İÇİN TEST İŞLEMLERİ YAN MENÜDE</h1>
+
+    
+
+    
+
+  </div>
+
+
+</body>
+
+</html>
---
+++ islemler.php
@@ -0,0 +1,38 @@
+<?php
+ 
+ 
+ switch(@$_GET["islem"]):
+ 
+ 
+ case "normal":
+ 
+ echo 'Selam';
+ 
+ break;
+ 
+ case "kayitlar":
+ 
+ echo 'veritabanı kayıtları ve işlemleri olacak';
+ 
+ break;
+ 
+ case "oturum":
+ 
+ echo 'Oturum İşlemleri olacak';
+ 
+ break;
+ 
+ case "dosyalar":
+ 
+ echo 'Dosyalar ile ilgili bilgi olacak';
+ 
+ break;
+ 
+ 
+ 
+ endswitch;
+ 
+ ?>
+
+    
+
